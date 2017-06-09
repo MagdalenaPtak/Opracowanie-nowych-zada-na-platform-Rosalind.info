4cdb8055ad1cdd6672c34f695ec70cb578a7dbe7
Magdalena Ptak <Magdalena.ptak@doctoral.uj.edu.pl>
2017-06-09 18:22:16 +0200

    - added test content

--- test/Task3/solver.py
+++ test/Task3/solver.py
@@ -1,83 +1,74 @@
 import pytest
 
 
-def test_can_import_Task2_solve():
-    from Task2.solver import solve
+def test_can_import_Task3_solve():
+    from Task3.solver import solve
 
 
-def test_task2_solver_raises_TypeError_when_not_recieving_string(task2_solver, task2_invalid_types):
+def test_task3_solver_raises_TypeError_when_not_recieving_string(task3_solver, task3_invalid_types):
     with pytest.raises(TypeError):
-        task2_solver(task2_invalid_types)
+        task3_solver(task3_invalid_types)
 
 
-def test_task2_solver_raises_Exception_when_recieving_string_not_divisiable_by_3(task2_solver):
-    for length in range(1, 99):
-        if not length % 3:
-            continue
-        input_tmp = "A" * length
-        with pytest.raises(Exception):
-            task2_solver(input_tmp)
+def test_task3_solver_accepts_proper_RNA_input_string(task3_solver):
+    assert task3_solver("AUCGAU")
 
 
-def test_task2_solver_accepts_proper_RNA_input_string(task2_solver):
-    assert task2_solver("AUCGAU")
+def test_task3_solver_accepts_generated_RNA_input_string(task3_solver, task3_generated_output):
+    assert task3_solver(task3_generated_output)
 
 
-def test_task2_solver_accepts_generated_RNA_input_string(task2_solver, task2_generated_output):
-    assert task2_solver(task2_generated_output)
+def test_task3_solver_generates_string_output(task3_solution):
+    assert isinstance(task3_solution, str)
 
 
-def test_task2_solver_generates_string_output(task2_solution):
-    assert isinstance(task2_solution, str)
-
-
-def test_task2_solver_generates_json_string(task2_solution):
+def test_task3_solver_generates_json_string(task3_solution):
     import json
-    assert json.loads(task2_solution)
+    assert json.loads(task3_solution)
 
 
-def test_task2_solver_generates_dict_after_loading_from_json(task2_solution):
+def test_task3_solver_generates_dict_after_loading_from_json(task3_solution):
     import json
-    output = json.loads(task2_solution)
+    output = json.loads(task3_solution)
     assert isinstance(output, dict)
 
 
-def test_task2_solver_generates_proper_dict(dict_task2_solution, task2_proper_dict_keys):
-    assert len(dict_task2_solution) == len(task2_proper_dict_keys)
-    for key in dict_task2_solution.keys():
-        assert key in task2_proper_dict_keys
+def test_task3_solver_generates_proper_dict(dict_task3_solution, task3_proper_dict_keys):
+    assert len(dict_task3_solution) == len(task3_proper_dict_keys)
+    for key in dict_task3_solution.keys():
+        assert key in task3_proper_dict_keys
 
 
 #######################################################################################################################
 
 @pytest.fixture()
-def task2_generated_output():
-    from Task2.generator import generate
+def task3_generated_output():
+    from Task3.generator import generate
     return generate()
 
 
 @pytest.fixture()
-def task2_solver():
-    from Task2.solver import solve
+def task3_solver():
+    from Task3.solver import solve
     return solve
 
 
 @pytest.fixture(params=[None, [], (), 1, object])
-def task2_invalid_types(request):
+def task3_invalid_types(request):
     return request.param
 
 
 @pytest.fixture(params=["Z", "()", "1", "object", "t", "", "AUCD"])
-def task2_invalid_strings(request):
+def task3_invalid_strings(request):
     return request.param
 
 
 @pytest.fixture()
-def dict_task2_solution(task2_solution):
+def dict_task3_solution(task3_solution):
     import json
-    return json.loads(task2_solution)
+    return json.loads(task3_solution)
 
 
 @pytest.fixture()
-def task2_proper_dict_keys():
-    return ["UAG", "UAA", "UGA"]
+def task3_proper_dict_keys():
+    return ["junction_type", "junction_count", "pseudoknot_complexity", "pseudoknot_count"]
