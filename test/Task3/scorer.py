4aa7586d65b27afc47a9b958e92569e2f01dbbeb
Magdalena Ptak <Magdalena.ptak@doctoral.uj.edu.pl>
2017-06-09 18:12:22 +0200

    - added content

--- test/Task3/scorer.py
+++ test/Task3/scorer.py
@@ -1,72 +1,72 @@
 import pytest
 
 
-def test_can_import_Task2_score():
-    from Task2.scorer import score
+def test_can_import_Task3_score():
+    from Task3.scorer import score
 
 
-def test_task2_scorer_raises_TypeError_when_not_recieving_string_as_inputs(task2_scorer, task2_invalid_inputs):
+def test_task3_scorer_raises_TypeError_when_not_recieving_string_as_inputs(task3_scorer, task3_invalid_inputs):
     with pytest.raises(TypeError):
-        task2_scorer(task2_invalid_inputs[0], task2_invalid_inputs[1], task2_invalid_inputs[2])
+        task3_scorer(task3_invalid_inputs[0], task3_invalid_inputs[1], task3_invalid_inputs[2])
 
 
 
-def test_task2_scorer_raises_TypeError_when_recieving_empty_strings_as_inputs(task2_scorer):
+def test_task3_scorer_raises_TypeError_when_recieving_empty_strings_as_inputs(task3_scorer):
     with pytest.raises(TypeError):
-        task2_scorer("", "", "")
+        task3_scorer("", "", "")
 
 
-def test_task2_scorer_raises_JSONDecodeError_when_recieving_imporper_strings(task2_scorer, task2_invalid_strings):
+def test_task3_scorer_raises_JSONDecodeError_when_recieving_imporper_strings(task3_scorer, task3_invalid_strings):
     import json
     try:
         exception = json.JSONDecodeError
     except AttributeError:
         exception = ValueError
     with pytest.raises(exception):
-        task2_scorer(task2_invalid_strings[0], task2_invalid_strings[1], task2_invalid_strings[2])
+        task3_scorer(task3_invalid_strings[0], task3_invalid_strings[1], task3_invalid_strings[2])
 
 
-def test_task2_scorer_can_accept_JSON_dict_strings(task2_scorer):
-    assert task2_scorer("ACUG", "{}", "{}")
+def test_task3_scorer_can_accept_JSON_dict_strings(task3_scorer):
+    assert task3_scorer("ACUG", "{}", "{}")
 
 
-def test_task2_scorer_can_accept_task2_generated_outputs(task2_scorer, task2_generated_output, task2_solution):
-    assert task2_scorer(task2_generated_output, task2_solution, task2_solution)
+def test_task3_scorer_can_accept_task3_generated_outputs(task3_scorer, task3_generated_output, task3_solution):
+    assert task3_scorer(task3_generated_output, task3_solution, task3_solution)
 
 
-def test_task2_scorer_fails_user_input_when_recieving_wrong_number_of_keys_in_user_input(task2_scorer, task2_generated_output, task2_solution):
-    wrong_task2_solution = task2_solution
+def test_task3_scorer_fails_user_input_when_recieving_wrong_number_of_keys_in_user_input(task3_scorer, task3_generated_output, task3_solution):
+    wrong_task3_solution = task3_solution
     import json
-    wrong_task2_solution = json.loads(wrong_task2_solution)
-    del wrong_task2_solution["UAG"]
-    wrong_task2_solution = json.dumps(wrong_task2_solution)
-    assert not task2_scorer(task2_generated_output, task2_solution, wrong_task2_solution)
+    wrong_task3_solution = json.loads(wrong_task3_solution)
+    del wrong_task3_solution["junction_count"]
+    wrong_task3_solution = json.dumps(wrong_task3_solution)
+    assert not task3_scorer(task3_generated_output, task3_solution, wrong_task3_solution)
 
-def test_task2_scorer_fails_user_input_when_recieving_not_matching_task2_solutions(task2_scorer, task2_generated_output, task2_solution):
-    wrong_task2_solution = task2_solution
+def test_task3_scorer_fails_user_input_when_recieving_not_matching_task3_solutions(task3_scorer, task3_generated_output, task3_solution):
+    wrong_task3_solution = task3_solution
     import json
-    wrong_task2_solution = json.loads(wrong_task2_solution)
-    wrong_task2_solution["UAG"].append("AAA")
-    wrong_task2_solution = json.dumps(wrong_task2_solution)
-    assert not task2_scorer(task2_generated_output, task2_solution, wrong_task2_solution)
+    wrong_task3_solution = json.loads(wrong_task3_solution)
+    wrong_task3_solution["junction_count"] += 1
+    wrong_task3_solution = json.dumps(wrong_task3_solution)
+    assert not task3_scorer(task3_generated_output, task3_solution, wrong_task3_solution)
 
 #######################################################################################################################
 
 @pytest.fixture()
-def task2_generated_output():
-    from Task2.generator import generate
+def task3_generated_output():
+    from Task3.generator import generate
     return generate()
 
 
 @pytest.fixture()
-def task2_solution(task2_generated_output):
-    from Task2.solver import solve
-    return solve(task2_generated_output)
+def task3_solution(task3_generated_output):
+    from Task3.solver import solve
+    return solve(task3_generated_output)
 
 
 @pytest.fixture()
-def task2_scorer():
-    from Task2.scorer import score
+def task3_scorer():
+    from Task3.scorer import score
     return score
 
 
@@ -75,7 +75,7 @@ def task2_scorer():
                         (1, 2, "TTT"),
                         ("1", None, ["AAA"])
                         ])
-def task2_invalid_inputs(request):
+def task3_invalid_inputs(request):
     return request.param
 
 
@@ -83,5 +83,5 @@ def task2_invalid_inputs(request):
                         ("1", "[]", "TTT"),
                         ("[]", "[111]", "AAA")
                         ])
-def task2_invalid_strings(request):
+def task3_invalid_strings(request):
     return request.param
--- test/__init__.py
+++ test/__init__.py
@@ -1,2 +1,3 @@
 from .Task1 import *
 from .Task2 import *
+from .Task3 import *
